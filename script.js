72dedff517b01ebbf61a61d6d40398f9f954b316
danielalvarez2000 <122405591+danielalvarez2000@users.noreply.github.com>
2023-02-14 16:45:41 -0800

    changes

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 // Check JS is Connected
 console.log('JS Connected');
+
+let tl = gsap.timeline();
+
+tl.to(".soccerchick", {
+    duration: 1,
+    x: 400,
+    scale: 0,
+    opacity: 0
+
+})
+
+    .set(".soccerchick", {
+        x: 400
+    })
+
+    .to(".soccerchick", {
+        duration: 1,
+        scale: 1,
+        opacity: 1
+    })
+
